055317f96400b6b4d0649af804cc1fd468f899e7
Fede Otálvares <fnotalvares@gmail.com>
2023-05-30 07:47:32 +0200

    Add Layout component and LayoutStyled

---
+++ src/components/Layout/LayoutStyled.ts
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+const LayoutStyled = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin: 0 auto;
+  align-items: center;
+  min-width: 320px;
+  max-width: 900px;
+`;
+
+export default LayoutStyled;
--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./components/App/App";
 import "./index.css";
-import { Provider } from "react-redux";
-import { store } from "./store";
+import { ThemeProvider } from "styled-components";
+import GlobalStyle from "./styles/GlobalStyle";
+import theme from "./styles/theme/theme";
+import App from "./components/App/App";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <Provider store={store}>
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
       <App />
-    </Provider>
+    </ThemeProvider>
   </React.StrictMode>
 );
