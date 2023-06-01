84865f9a3957229f7898b188747590a61110e7d8
Fede Otálvares <fnotalvares@gmail.com>
2023-06-01 13:35:15 +0200

    Add useToken custom hook

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,8 +1,14 @@
 import React from "react";
 import Layout from "../Layout/Layout";
+import { Provider } from "react-redux";
+import { store } from "../../store";
 
 const App = (): React.ReactElement => {
-  return <Layout />;
+  return (
+    <Provider store={store}>
+      <Layout />
+    </Provider>
+  );
 };
 
 export default App;
