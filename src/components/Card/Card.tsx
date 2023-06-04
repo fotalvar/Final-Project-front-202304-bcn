dbf2782393666b7569a814d47d1d9f3ccb6d90f3
Fede Otálvares <fnotalvares@gmail.com>
2023-06-04 15:07:29 +0200

    Add Card component

---
+++ src/components/Card/Card.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import CardStyled from "./CardStyled";
+import { TeamsStructure } from "../../store/teams/types";
+
+interface CardProps {
+  team: TeamsStructure;
+}
+
+const Card = ({ team }: CardProps): React.ReactElement => {
+  return (
+    <CardStyled>
+      <li className="teamCard" key={team.id}>
+        <article className="teamCard__container">
+          <button className="teamCard__close">
+            <img
+              src="../images/icons/remove.svg"
+              alt="remove icon"
+              width="13"
+              height="13"
+            />
+          </button>
+          <button className="teamCard__card">
+            <img
+              src={team.bgimage}
+              alt="Team"
+              className="teamCard__teamImage"
+            />
+            <h2 className="teamCard__title">{team.name}</h2>
+          </button>
+          <section className="teamCard__info">
+            <span className="teamCard__rating">{team.rating}</span>
+            <span className="teamCard__type">{team.type}</span>
+          </section>
+        </article>
+      </li>
+    </CardStyled>
+  );
+};
+
+export default Card;
