0d9656ade065b8166a592e9d8e665ee0c3c409ff
Fede Otálvares <fnotalvares@gmail.com>
2023-06-04 12:33:27 +0200

    Add styles to Button component

---
+++ src/components/Button/ButtonStyled.tsx
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+
+const ButtonStyled = styled.button`
+  width: 180px;
+  height: 55px;
+  text-transform: uppercase;
+  font-size: ${(props) => props.theme.fontSizes.normal};
+  border-radius: 50px;
+  border: none;
+  box-shadow: 0px 6px 6px #0000003f;
+
+  &__login {
+    background: ${(props) => props.theme.colors.primary};
+    color: ${(props) => props.theme.colors.mainLight};
+    transition: 0.5s;
+  }
+
+  &__loadMore {
+    background: ${(props) => props.theme.colors.secondary};
+    color: ${(props) => props.theme.colors.mainLight};
+    transition: 0.5s;
+  }
+`;
+
+export default ButtonStyled;
