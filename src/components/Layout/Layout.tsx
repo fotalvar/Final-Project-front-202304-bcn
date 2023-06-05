b30a600782119aef66e6c9b7ae369d0235057fa0
Fede Otálvares <fnotalvares@gmail.com>
2023-06-05 16:42:54 +0200

    Add styles to Loader component

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -3,12 +3,14 @@ import Header from "../Header/Header";
 import ContainerStyled from "../shared/ContainerStyled";
 import React from "react";
 import paths from "../../routers/paths/paths";
+import Loader from "../Loader/Loader";
 
 const Layout = (): React.ReactElement => {
   const location = useLocation();
 
   return (
     <ContainerStyled>
+      <Loader />
       {location.pathname === paths.login || <Header />}
       <Outlet />
     </ContainerStyled>
