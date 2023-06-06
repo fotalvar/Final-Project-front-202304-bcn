7027730b4cdd09bd4697d7ff92273aebb0e19c81
Fede Otálvares <fnotalvares@gmail.com>
2023-06-06 12:42:09 +0200

    Add uiSlice and mini reducers

--- src/store/ui/uiSlice.tsx
+++ src/store/ui/uiSlice.tsx
@@ -1,12 +1,13 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { PayloadAction, createSlice } from "@reduxjs/toolkit";
 import { UiStateStructure } from "../../types";
 
 const initialState: UiStateStructure = {
   isLoading: false,
+  isError: false,
 };
 
 const uiSlice = createSlice({
-  name: "loader",
+  name: "ui",
   initialState: initialState,
   reducers: {
     showLoader: (currentUiState: UiStateStructure) => ({
@@ -17,12 +18,25 @@ const uiSlice = createSlice({
       ...currentUiState,
       isLoading: false,
     }),
+    showError: (
+      currentUiState: UiStateStructure,
+      action: PayloadAction<UiStateStructure>
+    ) => ({
+      ...currentUiState,
+      isError: action.payload.isError,
+    }),
+    hideError: (currentUiState: UiStateStructure) => ({
+      ...currentUiState,
+      isError: false,
+    }),
   },
 });
 
 export const {
   showLoader: showLoaderActionCreator,
   hideLoader: hideLoaderActionCreator,
+  showError: showErrorActionCreator,
+  hideError: hideErrorActionCreator,
 } = uiSlice.actions;
 
 export const uiReducer = uiSlice.reducer;
