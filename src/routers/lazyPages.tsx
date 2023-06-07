14298c9daa160f73f0e61e9265f628f483f707a1
Fede Otálvares <fnotalvares@gmail.com>
2023-06-07 17:37:22 +0200

    Add /not-found path to router

--- src/routers/lazyPages.tsx
+++ src/routers/lazyPages.tsx
@@ -3,3 +3,7 @@ import { lazy } from "react";
 export const LazyLoginPage = lazy(() => import("../pages/LoginPage/LoginPage"));
 
 export const LazyListPage = lazy(() => import("../pages/ListPage/ListPage"));
+
+export const LazyErrorPage = lazy(
+  () => import("../pages/NotFoundPage/NotFoundPage")
+);
