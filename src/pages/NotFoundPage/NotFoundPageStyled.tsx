191dcecdfa92ea320efda499800c035a0a187818
Fede Otálvares <fnotalvares@gmail.com>
2023-06-07 17:38:45 +0200

    Add styles to 404 not found error page

---
+++ src/pages/NotFoundPage/NotFoundPageStyled.tsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+const NotFoundPageStyled = styled.main`
+  display: grid;
+  place-items: center;
+  text-align: center;
+  color: #ffffff7f;
+  text-shadow: 0px 4px 4px #0000003f;
+  font-size: ${(props) => props.theme.fontSizes.medium};
+  width: 250px;
+  height: fit-content;
+  margin-top: 20vh;
+`;
+export default NotFoundPageStyled;
