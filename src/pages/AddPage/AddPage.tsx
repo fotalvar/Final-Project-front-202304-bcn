85fe3bdc3f5c23372b6e517d8c7ca1968624a136
Fede Otálvares <fnotalvares@gmail.com>
2023-06-10 13:22:21 +0200

    Create AddPage

---
+++ src/pages/AddPage/AddPage.tsx
@@ -0,0 +1,11 @@
+import AddPageStyled from "./AddPageStyled";
+
+const AddPage = (): React.ReactElement => {
+  return (
+    <AddPageStyled>
+      <h1 className="add-team__title">Create Team</h1>;
+    </AddPageStyled>
+  );
+};
+
+export default AddPage;
